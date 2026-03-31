66d04920ed41246e4bb184394cc8525b0a20ef75
manurota <manuel.rota08@gmail.com>
2026-03-31 14:45:18 +0200

    aggiunta tabella base base

--- react-pcto/src/App.jsx
+++ react-pcto/src/App.jsx
@@ -1,21 +1,12 @@
 import './App.css'
 import Card from './components/Card'
-import ferrari from './assets/ferrari.jpg'
-import lamborghini from './assets/lamborghini.jpg'
+import Tabella from './components/Tabella'
+
 function App(){
   return(
     <>
-      
-        <Card
-        title={"Ferrari"}
-        description={"Questa è una ferrari"}
-        imgUrl={ferrari}
-        ></Card>
-        <Card
-        title={"Lamborghini"}
-        description={"Questa è una lamborghini"}
-        imgUrl={lamborghini}
-        ></Card>
+      <Tabella></Tabella>
+        
 
     
     </>
--- react-pcto/src/components/Tabella.jsx
+++ react-pcto/src/components/Tabella.jsx
@@ -1,10 +1,23 @@
+import "./Tabella.css"
 function Tabella(){
     return(
-        <div>
-            
+        <div id="tabella">
+            <table>
+                <thead>
+                    <tr><td>Questo è il titolo</td></tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>R1 C1</td>
+                        <td>R1 C2</td>
+                    </tr>
+                    <tr>
+                        <td>R2 C1</td>
+                        <td>R2 C2</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-
     )
-
 }
 export default Tabella
